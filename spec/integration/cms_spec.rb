1b15936dcf96991489e63eb23fa85c60bab78fd2
Andrew Mackenzie <andrew@mackenzie-serres.net>
2020-01-19 18:02:57 +0100

    Added a test for cms sections and disabled other tests until decide final implementation

--- spec/integration/cms_spec.rb
+++ spec/integration/cms_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe 'CMS API', type: :integration do
   context '#list_cms_files' do
     it { expect(client.list_cms(:file, 1).length).to be >= 1 }
   end
+
+  context '#list_cms_sections' do
+    it { expect(client.list_cms(:section, 1).length).to be >= 1 }
+  end
+
+  context '#list_cms_partials', skip: true do
+    it { expect(client.list_cms(:template, 1, 'partial').length).to be >= 1 }
+  end
+
+  context '#list_cms_pages', skip: true do
+    it { expect(client.list_cms(:template, 1, :page).length).to be >= 1 }
+  end
 end
